284d56a812c10b7c6510598351bcf438719a8417
Dhruv Dankhara <dhruvdankhara09@gmail.com>
2024-01-01 21:50:32 +0530

    add day 18 19

---
+++ Lec-18 While Loops/main.py
@@ -0,0 +1,22 @@
+# i = 0
+# while(i < 3):
+#     print(i)
+#     i = i + 1
+
+# i = 5
+# while(i > 0):
+#     print(i)
+#     i -= 1
+
+# a = int(input("enter number"))
+# while(a != 100):
+#     a = int(input("enter number"))
+#     print(a)
+
+# else execute after while loop
+i = 0
+while(i < 5):
+    print(i)
+    i += 1
+else:
+    print("in else")
